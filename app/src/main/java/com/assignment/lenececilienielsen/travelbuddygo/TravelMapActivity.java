a690361705757355d0dbed27fa16e1e519a9ee07
Kim-Erik <kim.lundberg@hig.no>
2016-10-07 14:12:01 +0200

    Clicking on tags in the map now displays an image, title, description, latitude and longditude.

--- app/src/main/java/com/assignment/lenececilienielsen/travelbuddygo/TravelMapActivity.java
+++ app/src/main/java/com/assignment/lenececilienielsen/travelbuddygo/TravelMapActivity.java
@@ -7,12 +7,15 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.io.IOException;
@@ -69,12 +72,89 @@ public class TravelMapActivity extends FragmentActivity implements OnMapReadyCal
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        /**
+         * Tutorial used for the info window functionality.
+         * URL: https://www.youtube.com/watch?v=g7rvqxn8SLg
+         */
+        if(mMap != null) {
+            mMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
+                public View getInfoWindow(Marker arg0) {
+                    return null;
+                }
+
+                public View getInfoContents(Marker marker) {
+                    View v = getLayoutInflater().inflate(R.layout.custom_info_contents, null);
+
+                    TextView tvLocality = (TextView) v.findViewById(R.id.tv_locality);
+                    TextView tvLat = (TextView) v.findViewById(R.id.tv_lat);
+                    TextView tvLng = (TextView) v.findViewById(R.id.tv_lng);
+                    TextView tvSnippit = (TextView) v.findViewById(R.id.tv_snippet);
+
+                    LatLng ll = marker.getPosition();
+                    tvLocality.setText(marker.getTitle());
+                    tvLat.setText("Latitude: " + ll.latitude);
+                    tvLng.setText("Longitude: " + ll.longitude);
+                    tvSnippit.setText(marker.getSnippet());
+                    return v;
+                }
+            });
+        }
+
+        /**
+         * Tutorial used for the info window functionality.
+         * URL: https://developers.google.com/maps/documentation/android-api/marker
+         */
+        LatLng SYDNEY = new LatLng(-33.865081, 151.203843);
+        LatLng BRISBANE = new LatLng(-27.47093, 153.0235);
+        LatLng NEWYORK = new LatLng(40.714778, -74.011017);
+        LatLng PARIS = new LatLng(48.856353, 2.352705);
+        LatLng NTNU = new LatLng(60.788292, 10.680422);
+
+        Marker mSYDNEY;
+        Marker mBrisbane;
+        Marker mNewYork;
+        Marker mParis;
+        Marker mNTNU;
+
+        mSYDNEY = mMap.addMarker(new MarkerOptions()
+                .position(SYDNEY)
+                .title("Sydney")
+                .snippet("Facts about Sydney here."));
+        mSYDNEY.setTag(0);
+
+        mBrisbane = mMap.addMarker(new MarkerOptions()
+                .position(BRISBANE)
+                .title("Brisbane")
+                .snippet("Facts about Brisbane here."));
+        mBrisbane.setTag(0);
+
+        mNewYork = mMap.addMarker(new MarkerOptions()
+                .position(NEWYORK)
+                .title("New York")
+                .snippet("Facts about New York here."));
+        mNewYork.setTag(0);
+
+        mParis = mMap.addMarker(new MarkerOptions()
+                .position(PARIS)
+                .title("Paris")
+                .snippet("Facts about Paris here."));
+        mParis.setTag(0);
+
+        mNTNU = mMap.addMarker(new MarkerOptions()
+                .position(NTNU)
+                .title("NTNU Gjøvik")
+                .snippet("Facts about NTNU here."));
+        mNTNU.setTag(0);
+
+
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(NTNU));
+        mMap.setMyLocationEnabled(true);
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
+        /*LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
         // Adds a small button that finds your current location.
-        mMap.setMyLocationEnabled(true);
-
+        mMap.setMyLocationEnabled(true);*/
     }
 }
