65cd29cb1f266847d6d878c75f3c6f98c8d940a6
Kim-Erik <kim.lundberg@hig.no>
2016-10-10 13:51:10 +0200

    Added more locations to the map

--- app/src/main/java/com/assignment/lenececilienielsen/travelbuddygo/TravelMapActivity.java
+++ app/src/main/java/com/assignment/lenececilienielsen/travelbuddygo/TravelMapActivity.java
@@ -109,12 +109,20 @@ public class TravelMapActivity extends FragmentActivity implements OnMapReadyCal
         LatLng NEWYORK = new LatLng(40.714778, -74.011017);
         LatLng PARIS = new LatLng(48.856353, 2.352705);
         LatLng NTNU = new LatLng(60.788292, 10.680422);
+        LatLng CAPETOWN = new LatLng(-33.959104, 18.416973);
+        LatLng RIODEJANEIRO = new LatLng(-22.907193, -43.171657);
+        LatLng BEIJING = new LatLng(39.905530, 116.335169);
+        LatLng SAINTPETERSBURG = new LatLng(59.940840, 30.332349);
 
         Marker mSYDNEY;
         Marker mBrisbane;
         Marker mNewYork;
         Marker mParis;
         Marker mNTNU;
+        Marker mBeijing;
+        Marker mSaintPetersburg;
+        Marker mCapeTown;
+        Marker mRioDeJaneiro;
 
         mSYDNEY = mMap.addMarker(new MarkerOptions()
                 .position(SYDNEY)
@@ -125,27 +133,50 @@ public class TravelMapActivity extends FragmentActivity implements OnMapReadyCal
         mBrisbane = mMap.addMarker(new MarkerOptions()
                 .position(BRISBANE)
                 .title("Brisbane")
-                .snippet("Information about Brisbane here."));
+                .snippet("Brisbane is the third most populous city in Australia and has a population of 2.3 million. Brisbane is the fastest growing city in Australia. The Brisbane River flows through the city."));
         mBrisbane.setTag(0);
 
         mNewYork = mMap.addMarker(new MarkerOptions()
                 .position(NEWYORK)
                 .title("New York")
-                .snippet("Information about New York here."));
+                .snippet("The City of New York, often called New York City, is the most populous city in the United States. Major tourist destinations include Times Square; Broadway theater productions; the Empire State Building; the Statue of Liberty; Ellis Island; theUnited Nations Headquarters; museums such as the Metropolitan Museum of Art; greenspaces such as Central Park and Washington Square Park; Rockefeller Center.\n"));
         mNewYork.setTag(0);
 
         mParis = mMap.addMarker(new MarkerOptions()
                 .position(PARIS)
                 .title("Paris")
-                .snippet("Information about Paris here."));
+                .snippet("Paris is the capital and the most populous city of France. Paris is located in northern central France. The centre of Paris contains the most visited monuments in the city, including the Notre Dame Cathedral and the Louvre as well as theSainte-Chapelle; Les Invalides, where the tomb of Napoleon is located, and the Eiffel Tower are located on the Left Bank south-west of the centre.\n"));
         mParis.setTag(0);
+
+        mCapeTown = mMap.addMarker(new MarkerOptions()
+                .position(CAPETOWN)
+                .title("CapeTown")
+                .snippet("Cape Town is a coastal city in South Africa. It is the second most populous urban area in South Africa, after Johannesburg. The city is famous for its harbour, for its natural setting in the Cape Floristic Region, as well as for such well-known landmarks as Table Mountainand Cape Point."));
+        mCapeTown.setTag(0);
+
+        mRioDeJaneiro = mMap.addMarker(new MarkerOptions()
+                .position(RIODEJANEIRO)
+                .title("RioDeJaneiro")
+                .snippet("Rio is the second-most populous city in Brazil. Founded in 1565 by the Portuguese, the city was initially the seat of the Captaincy of Rio de Janeiro, a domain of the Portuguese Empire. Rio de Janeiro is one of the most visited cities in the Southern Hemisphere and is known for its natural settings, Carnival, samba, bossa nova, and balneario beaches such as Barra da Tijuca, Copacabana, Ipanema, and Leblon."));
+        mRioDeJaneiro.setTag(0);
 
         mNTNU = mMap.addMarker(new MarkerOptions()
                 .position(NTNU)
                 .title("NTNU Gjøvik")
-                .snippet("Information about NTNU here."));
+                .snippet("Gjøvik is a town in Oppland County in Norway, and Norway´s biggest lake “Mjøsa” is located here, there is also a department of the NTNU university located in Gjøvik."));
         mNTNU.setTag(0);
 
+        mBeijing = mMap.addMarker(new MarkerOptions()
+                .position(BEIJING)
+                .title("Bejing")
+                .snippet("Beijing (formerly romanized as Peking) is the capital of the People's Republic of China and the world's third most populous city proper. It is also the world's most populous capital city."));
+        mBeijing.setTag(0);
+
+        mSaintPetersburg = mMap.addMarker(new MarkerOptions()
+                .position(SAINTPETERSBURG)
+                .title("Saint Petersburg")
+                .snippet("Saint Petersburg is Russia's second-largest city after Moscow, with five million inhabitants in 2012 and an important Russian port on the Baltic Sea."));
+        mSaintPetersburg.setTag(0);
 
 
         /*
